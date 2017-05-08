04f6b245271ca36313bcf614614c2ffc2b0b46bd
Bruno P.S <bruno.ps@dcc.ufmg.br>
2017-05-08 16:08:05 -0300

    Commit only with tests

--- src/projects/mobility2cooja/nodes/nodeImplementations/Mote.java
+++ src/projects/mobility2cooja/nodes/nodeImplementations/Mote.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 
 import projects.mobility2cooja.LogL;
 import projects.mobility2cooja.models.mobilityModels.OfficeMobility;
+import sinalgo.configuration.Configuration;
 import sinalgo.configuration.WrongConfigurationException;
 import sinalgo.gui.transformation.PositionTransformation;
 import sinalgo.nodes.Node;
@@ -31,10 +32,20 @@ public class Mote extends Node {
 	@Override
 	public void init() {
 		
+		
+		if (this.ID == 1) {
+
+			try {
+				String place = Configuration.getStringParameter("OfficeMobility/SinkPlacement");
+				setPlacement(place);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		homePosition.assign(this.getPosition());
 		//Set initial position
 		if(OfficeMobility.mobilityLogging != null){
-			OfficeMobility.mobilityLogging.log(LogL.mobilityLog, (this.ID - 1) + " ");
+			OfficeMobility.mobilityLogging.log(LogL.mobilityLog, (this.ID - 1) + "");
 			OfficeMobility.mobilityLogging.log(LogL.mobilityLog, " 0 ");
 			OfficeMobility.mobilityLogging.logln(LogL.mobilityLog, homePosition.xCoord + " " + homePosition.yCoord);
 		}
@@ -72,7 +83,32 @@ public class Mote extends Node {
 	public void setHomePosition(Position homePosition) {
 		this.homePosition = homePosition;
 	}
+	
+		
+	private void setPlacement(String place) {
+		switch (place) {
+		case "CENTER":
+			this.setPosition(Configuration.dimX / 2, Configuration.dimY / 2, Configuration.dimZ / 2);
+			break;
+		case "CORNER":
+			this.setPosition(0, 0, 0);
+			break;
+		case "RANDOM":
+			// Random r = new Random();
+			// this.setPosition(r.getNextPosition());
+			break;
 
+		default:
+			// this.setPosition(0, 0, 0);
+			break;
+		}
+	}
+	
+	@NodePopupMethod(menuText = "Degree")
+	public void sendEventPopupMethod() {
+		Tools.appendToOutput("Node "+this.ID+" degree "+this.outgoingConnections.size()+"\n");
+	}
+	
 	@Override
 	public void draw(Graphics g, PositionTransformation pt, boolean highlight) {
 		// TODO Auto-generated method stub
